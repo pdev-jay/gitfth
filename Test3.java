c8d71cf66c612a954725a0dbcf796650953c461b
pdev.jay <pdev.jay@gmail.com>
2022-03-03 10:35:12 +0900

    [update] 5 Test3.java

---
+++ Test3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args){
+        int number1 = 0;
+        int number2 = 0;
+
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("input number 1 >> ");
+        number1 = scan.nextInt();
+    }
+}
---
+++ Test4.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Test4 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    }
+}
